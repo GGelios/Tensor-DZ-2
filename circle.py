cf1a4f44c09f8ab52018236cd29341bba8e0852b
GGelios <119353367+GGelios@users.noreply.github.com>
2022-12-01 20:03:38 +0300

    Add files via upload

---
+++ circle.py
@@ -0,0 +1,4 @@
+import math
+r=int(input('Введите радиус круга ', ))
+S=math.pi*r**2
+print('Площадь круга =', S)
---
+++ vichileniya.py
@@ -0,0 +1,10 @@
+a=float(input("Введите число "))
+b=float(input("Введите число "))
+c_0=a+b
+c_1=a-b
+c_2=a*b
+c_3=a/b
+c_4=a**b
+c_5=a%b
+c_6=a//b
+print (f'a+b=', c_0, 'a-b=', c_1, 'a*b=', c_2, 'a/b=', c_3, 'a**b=', c_4, 'a%''b=', c_5, 'a//b=', c_6)
